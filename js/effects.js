dbdf3d28a0fef100fd7c8fa230de51c740376383
chirag5624 <chirag5624@gmail.com>
2016-02-05 01:50:01 +0530

    aded hyperlinks in side menu

--- js/effects.js
+++ js/effects.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
    // $("#userId").addClass("hide");
-    $("#userId").click(function(){
-        $("#profile").fadeToggle();
-    });
+    
     $("aside>ul>li>a").each(function(){
             
            
@@ -24,11 +22,45 @@ $(document).ready(function(){
        }
     );
     
-   
+   $("#userId").click(function(){
+            $("#profile").fadeToggle();
+       
+        });
+    
     var options = "";
     for(var i = 1; i<11; i++ ) {
         options += "<option value="+i+">"+i+"</option>";
     }
     $("select[name='frequency']").html( options );
+     $("#profile").css("display","none");
+//    $(":not(.magic)").click(function(){
+//        if( $("#profile").css("display") == "block"){
+//                $("#profile").css("display","none");
+//            }
+////       / console.log("clicked"+ this);
+//        
+//    });
+     $(document).on('click', function(e) {
+        if (e.target.id == 'userId' || e.target.id == 'profile' || e.target.id == 'profile_email' || e.target.id == 'profile_options'|| e.target.id == 'profile_info' ) {
+            //
+            $('#profile').show();
+        }  
+         else{
+            $('#profile').hide();
+            //console.log(e.target.id + ' Clicked !!'); 
+        }
+
+    });
+    
+//    $("#profile").click(function(){
+//                     console.log("clicked "+ this.id); 
+//    });
+    $( document ).on( 'keydown', function ( e ) {
+        if ( e.keyCode === 27 ) {
+            $("#profile").hide();
+
+        }
+    
+    });
     
 });
